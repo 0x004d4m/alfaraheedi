abc37cffd907a86a77414629a3f0eae26bc38e8b
0x004d4m <0x004d4m@gmail.com>
2023-02-18 15:01:56 +0300

    updates

--- database/migrations/2022_11_09_144844_create_customer_reset_passwords_table.php
+++ database/migrations/2022_11_09_144844_create_customer_reset_passwords_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('customer_id');
             $table->foreign('customer_id')->references('id')->on('customers');
-            $table->string('token');
+            $table->string('token')->nullable();
             $table->timestamps();
         });
     }
