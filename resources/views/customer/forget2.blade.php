e8ffaf3ef3ab813a0e5f48b5b4f9c36e3b107749
0x004d4m <0x004d4m@gmail.com>
2023-02-18 15:31:42 +0300

    updates

--- resources/views/customer/forget2.blade.php
+++ resources/views/customer/forget2.blade.php
@@ -1,14 +1,14 @@
 @extends('website.layout.main')
 @section('title', __('content.signin'))
 @section('content')
-
 <!-- login Area Start -->
 <div class="login-form-area" dir="{{__('content.dir')}}">
-    <form class="login-form" method="POST" action="">
+    <form class="login-form" method="POST" action="/login">
         @csrf
         <!-- Login Heading -->
         <div class="login-heading">
-            <span>{{__('content.ForgotPassword')}}</span>
+            <span>{{__('content.Login')}}</span>
+            <p>{{__('content.Login2')}}</p>
             @if (Session::has('Error'))
                 <div class="alert alert-danger" role="alert">
                     {{ Session::get('Error') }}
@@ -18,14 +18,12 @@
                 @endphp
             @endif
         </div>
-        <div class="single-input-fields">
-            <label style="text-align: {{__('content.dir2')}};">{{__('content.Password')}}</label>
-            <input required type="password" name="password" placeholder="{{__('content.Password')}}">
-            @isset($Error)
-                @isset($Error->password)
-                    <small class="text-danger">{{ $Error->password[0] }}</small>
-                @endisset
-            @endisset
+        <!-- Single Input Fields -->
+        <div class="input-box">
+            <div class="single-input-fields">
+                <label style="text-align: {{__('content.dir2')}};">{{__('content.Password')}}</label>
+                <input type="password" name="password" required placeholder="{{__('content.Password')}}">
+            </div>
         </div>
         <!-- form Footer -->
         <div class="login-footer">
